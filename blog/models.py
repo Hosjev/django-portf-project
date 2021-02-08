d4b8b3085d3b649257d7cd724dac1079556944bb
Hosjev <hosjev@gmail.com>
2021-02-08 14:10:41 -0800

    created details; made things look pretty

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,14 @@ class Blog(models.Model):
     creation_date = models.DateTimeField()
     image = models.ImageField(upload_to="images/blog")
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        sum_words = 20
+        summarized = self.body.split(" ")
+        return " ".join(summarized[:sum_words]) + "..."
+
+    def pretty_date(self):
+        return self.creation_date.strftime("%b %e %Y")
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,8 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / 'portfolio/static/')
+]
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
-
-MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_URL = '/media/'
